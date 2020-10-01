7ac656e389f7880ba271a26fe28716900c28cf04
Radmir Khusnutdinov <v-rakhus@microsoft.com>
2020-10-01 15:07:57 +0000

    first commit

---
+++ FirstCSProj/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SecondCSProj;
+
+namespace FirstCSProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = SayHello.GetMessage();
+            Console.WriteLine(msg);
+        }
+    }
+}
